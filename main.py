27348dcd716226e42f02b780f18ba9ab46450bea
carl-bredly <107316043+condor30@users.noreply.github.com>
2024-02-25 22:35:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import json
+from CTkMessagebox import CTkMessagebox
+import customtkinter as ctk
+from app import App
+
+# Run app
+if __name__ == "__main__":
+    root = ctk.CTk()
+    app = App(root)
+    root.mainloop()
