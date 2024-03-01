13d4f36e5538310649a1fae1e83d9024cef29d5f
carl-bredly <107316043+condor30@users.noreply.github.com>
2024-03-01 23:59:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from CTkMessagebox import CTkMessagebox
 import customtkinter as ctk
 from app import App
-
+#default password is "1234"
 # Run app
 if __name__ == "__main__":
     root = ctk.CTk()
